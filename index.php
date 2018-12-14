b5649b8eb9c6a7fd8e3772303b0a61bd47a4b83f
skeptycal <skeptycal@gmail.com>
2018-12-14 14:12:36 -0600

    compartmentalize headers/footers to php includes

--- index.php
+++ index.php
@@ -3,17 +3,8 @@
 <html lang="en-US">
 
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width,initial-scale=1">
-    <!-- <base href="http://www.skeptycal.com/" target="_top"> -->
+    <?php include '/src/head_bp.php' ?>
     <title>Michael Treanor</title>
-
-    <link rel="stylesheet prefetch" href="https://fonts.googleapis.com/css?family=Muli:200,900">
-    <link rel="stylesheet" href="/css/normalize-8-0.css">
-    <link rel="stylesheet" href="/css/index.css">
-
-    <script type="text/javascript" src="https://platform.linkedin.com/badges/js/profile.js" async defer>
-    </script>
     <style type="text/css">
         .LI-profile-badge {
             border-radius: 25%;
@@ -22,18 +13,6 @@
             bottom: 3%;
         }
     </style>
-
-    <!-- 
-			current image: https://www.nasa.gov/sites/default/files/images/672309main_M107_full.jpg Courtesy of NASA.
-
-			secondary image: An infrared image showing fledgling stars located in the Orion Nebula. Created: 29 February 2012. 
-			<a href="http://www.nasa.gov/multimedia/imagegallery/image_feature_2195.html">Image credit: NASA</a>
-			
-			Icons and other resources by Rawpixel.com and Ydlabs, among others. 
-			<a href="https://www.freepik.com/free-photos-vectors/background">Freepik.com</a>
-
-			Artistic site design inspiration by <a href="https://github.com/saracope">Sara Cope</a>.
-		-->
 </head>
 
 <body>
---
+++ src/head_bp.php
@@ -0,0 +1,18 @@
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width,initial-scale=1">
+<!-- <base href="http://www.skeptycal.com/" target="_top"> -->
+<link rel="stylesheet prefetch" href="https://fonts.googleapis.com/css?family=Muli:200,900">
+<link rel="stylesheet" href="/css/normalize-8-0.css">
+<link rel="stylesheet" href="/css/index.css">
+<script type="text/javascript" src="https://platform.linkedin.com/badges/js/profile.js" async defer></script>
+<!--        
+    current image: https://www.nasa.gov/sites/default/files/images/672309main_M107_full.jpg Courtesy of NASA.
+    
+    secondary image: An infrared image showing fledgling stars located in the Orion Nebula. Created: 29 February 2012. 
+    <a href="http://www.nasa.gov/multimedia/imagegallery/image_feature_2195.html">Image credit: NASA</a>
+                
+    Icons and other resources by Rawpixel.com and Ydlabs, among others. 
+    <a href="https://www.freepik.com/free-photos-vectors/background">Freepik.com</a>
+
+    Artistic site design inspiration by <a href="https://github.com/saracope">Sara Cope</a>.
+     -->
